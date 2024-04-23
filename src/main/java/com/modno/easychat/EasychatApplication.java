c997e8d348f79566b232f5f6713eeb74f315e50b
easychat <icey_code@foxmail.com>
2024-04-23 14:04:23 +0800

    构建

---
+++ src/main/java/com/modno/easychat/EasychatApplication.java
@@ -0,0 +1,13 @@
+package com.modno.easychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasychatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasychatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/modno/easychat/EasychatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modno.easychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasychatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
